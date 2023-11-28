d58e9cca1ff24db1a17fb426af2032efc655f5cc
Souei08 <jubet.sode.5@gmail.com>
2023-11-28 10:11:31 +0800

    updates

--- server/controller/products.controller.js
+++ server/controller/products.controller.js
@@ -17,11 +17,62 @@ export const createProducts = async (req, res) => {
   const newProduct = new Products(productData);
 
   try {
-    await newProduct.delete();
+    await newProduct.create();
     res.status(201).json(newProduct);
   } catch (error) {
     res.status(409).json({
       message: error.message,
     });
+  }
+};
+
+export const deleteProducts = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const foundUser = await Products.findOne({ _id: id });
+
+    if (foundUser || foundUser.length == 0) {
+      const response = await foundUser.deleteOne({ _id: id });
+      res.status(202).json(response);
+    } else {
+      res.status(404).json({ message: `User not found.` });
+    }
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
+};
+
+export const updateProducts = async (req, res) => {
+  const itemId = req.params.id;
+  const updatedData = req.body;
+
+  try {
+    const updatedItem = await Products.findByIdAndUpdate(itemId, updatedData, {
+      new: true,
+    });
+    res.json(updatedItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+export const getOneProducts = async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const product = await Products.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
 };
--- server/routes/products.js
+++ server/routes/products.js
@@ -2,11 +2,17 @@ import express from 'express';
 import {
   getProducts,
   createProducts,
+  deleteProducts,
+  updateProducts,
+  getOneProducts,
 } from '../controller/products.controller.js';
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.get('/:id', getOneProducts);
 router.post('/create', createProducts);
+router.delete('/delete/:id', deleteProducts);
+router.put('/update/:id', updateProducts);
 
 export default router;
